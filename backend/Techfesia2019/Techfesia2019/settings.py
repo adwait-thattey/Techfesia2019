de195376364eda0050da094e1bb34e4e6132c8ab
Sayam753 <sayamkumar753@yahoo.in>
2019-06-05 19:11:38 +0530

    modify installed apps in setting.py

--- backend/Techfesia2019/Techfesia2019/settings.py
+++ backend/Techfesia2019/Techfesia2019/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accouts.apps.AccoutsConfig',
+    'base.apps.BaseConfig',
+    'events.apps.EventsConfig',
+    'auth.apps.AuthConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
