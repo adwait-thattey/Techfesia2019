58fa8baf9a6c80146d7a468865f03715d34f4482
adwait-thattey <adwaitthattey@gmail.com>
2019-06-14 15:45:23 +0530

    setup custom User Model

--- backend/Techfesia2019/Techfesia2019/settings.py
+++ backend/Techfesia2019/Techfesia2019/settings.py
@@ -112,6 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'registration.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
--- backend/Techfesia2019/registration/models.py
+++ backend/Techfesia2019/registration/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    """
+    Extended from Abstract User
+    List of inherited fields: first_name, last_name, email, username, password
+    """
