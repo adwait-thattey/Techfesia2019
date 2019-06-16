71269bbea96d431a75c2b27fccd6774e699fbc73
adwait-thattey <coderdude1999@gmail.com>
2019-06-16 14:13:56 +0530

    change order of auth classes to ensure 401 is sent instead of 403

--- backend/Techfesia2019/Techfesia2019/settings.py
+++ backend/Techfesia2019/Techfesia2019/settings.py
@@ -133,8 +133,9 @@ REST_FRAMEWORK = {
     # ]
     'DEFAULT_AUTHENTICATION_CLASSES': (
 
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+
     )
 }
 
