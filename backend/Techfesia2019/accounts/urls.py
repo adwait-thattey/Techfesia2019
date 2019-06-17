6a7bb371f8d92ffb00829bae41dc7bae2df72151
adwait-thattey <coderdude1999@gmail.com>
2019-06-17 11:07:39 +0530

    add email confirmation functionality

--- backend/Techfesia2019/accounts/urls.py
+++ backend/Techfesia2019/accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name="accounts"
 
 urlpatterns = [
-    path('<str:username>/email_confirmation', views.EmailConfirmed.as_view(), name="email_confirmed")
+    path('<str:username>/email_confirmation', views.EmailConfirmed.as_view(), name="email_confirmed"),
+    path('<str:username>/activate/<uidb64>/<token>', views.activate, name="activate_account" )
 ]
--- backend/Techfesia2019/accounts/views.py
+++ backend/Techfesia2019/accounts/views.py
@@ -1,12 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
 from django.utils.decorators import method_decorator
+from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from base.utils import account_activation_token
 from registration.decorators import is_user_calling_self
 from registration.models import User
 
@@ -33,3 +37,20 @@ class EmailConfirmed(APIView):
         #                         data={"message": "You do not have permission to perform this action"})
 
         return Response(status=status.HTTP_200_OK, data={"email_confirmed": user.email_confirmed})
+
+
+
+def activate(request, username, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.email_confirmed = True
+        user.save()
+        return HttpResponse("Your email was successfully confirmed. You may go ahead and login now")
+    else:
+        return HttpResponse("Invalid token!")
