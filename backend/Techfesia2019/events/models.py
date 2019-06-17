f73b7c1fecc667f70c9f7f33330f9e54cc7f86d8
adwait-thattey <coderdude1999@gmail.com>
2019-06-17 12:45:28 +0530

    add 2 fields to event model

--- backend/Techfesia2019/events/models.py
+++ backend/Techfesia2019/events/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 
 # Create your models here.
+from base.utils import generate_random_string
+
 
 class Event(models.Model):
-    pass
+    public_id = models.CharField(max_length=100, unique=True, blank=True)
+    title = models.CharField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.public_id:
+            self.public_id = generate_random_string()
+
+        super().save(*args, **kwargs)
