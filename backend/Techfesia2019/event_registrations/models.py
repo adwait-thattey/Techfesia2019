411a560f1f154c475ed94114914a9af56ccad31e
adwait-thattey <coderdude1999@gmail.com>
2019-06-23 12:16:05 +0530

    add team registration models

--- backend/Techfesia2019/event_registrations/models.py
+++ backend/Techfesia2019/event_registrations/models.py
@@ -77,11 +77,49 @@ class SoloEventRegistration(models.Model):
 
         if hasattr(self.profile, 'profileorganizer'):
 
-           if self.profile.profileorganizer in  self.event.organizers.all():
-                pass
+            if self.profile.profileorganizer in self.event.organizers.all():
+                raise ValidationError(_("Organizer can not be a participant for the same event"))
 
-        event_volunteers = self.event.volunteers.all()
+        if hasattr(self.profile, 'profilevolunteer'):
 
+            if self.profile.profilevolunteer in self.event.volunteers.all():
+                raise ValidationError(_("Volunteer can not be a participant for the same event"))
+
+    def save(self, *args, **kwargs):
+        if not self.public_id:
+            self.public_id = generate_public_id(self)
+
+        super().save(*args, **kwargs)
+
+
+class TeamEventRegistration(models.Model):
+    public_id = models.CharField(max_length=100,
+                                 unique=True,
+                                 blank=True,
+                                 db_index=True)
+
+    event = models.ForeignKey(to=Event, on_delete=models.PROTECT)
+
+    team = models.ForeignKey(to=Team, on_delete=models.CASCADE)
+
+    is_complete = models.BooleanField(default=False,
+                                      help_text="Tells whether the team has completed all formalities like payments etc."
+                                      )
+
+    is_confirmed = models.BooleanField(default=False,
+                                       help_text="Tells whether registration is confirmed or is in waiting"
+                                       )
+
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        if self.is_complete is False and self.is_confirmed is True:
+            raise ValidationError(_("Registration can not be confirmed until it is complete"))
+
+       #TODO: A check required that ensures no team member is an organizer/volunteer for same event
+       # Doing it here however might be too expensive
 
     def save(self, *args, **kwargs):
         if not self.public_id:
