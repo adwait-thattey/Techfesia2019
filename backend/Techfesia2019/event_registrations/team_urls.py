397ccafc5759df7ed144327996d97be6a18663ad
Ashutosh Chauhan <masterashu@live.in>
2019-06-27 16:35:14 +0530

    Adding names to url paths( Team List/Detail View)

--- backend/Techfesia2019/event_registrations/team_urls.py
+++ backend/Techfesia2019/event_registrations/team_urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import TeamDetailView, TeamListView
 
 urlpatterns = [
-    path('<slug:public_id>/', TeamDetailView.as_view()),
-    path('', TeamListView.as_view())
+    path('<slug:public_id>/', TeamDetailView.as_view(), name='team_details'),
+    path('', TeamListView.as_view(), name='teams_list'),
 ]
