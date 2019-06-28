ac244ae77531192a0b828d14d59a6c6b44e0ec83
Ashutosh Chauhan <masterashu@live.in>
2019-06-28 22:50:47 +0530

    Adding TicketComment Serializer

--- backend/Techfesia2019/tickets/serializers.py
+++ backend/Techfesia2019/tickets/serializers.py
@@ -5,11 +5,24 @@ from .models import TicketComment, Ticket
 class TicketSerializer(serializers.ModelSerializer):
     opened_by = serializers.SlugRelatedField(read_only=True, slug_field='get_user_username')
     solved_by = serializers.SlugRelatedField(read_only=True, slug_field='get_user_username')
+    subscribers = serializers.SlugRelatedField(many=True, read_only=True, slug_field='get_user_username')
 
     class Meta:
         model = Ticket
         fields = [
             'public_id', 'title', 'description',
             'opened_by', 'opening_date', 'status',
-            'solved_by', 'solving_date', 'content'
+            'solved_by', 'solving_date', 'content', 'subscribers'
+        ]
+
+
+class TicketCommentSerializer(serializers.ModelSerializer):
+    ticket = serializers.SlugRelatedField(read_only=True, slug_field='public_id')
+    commenter = serializers.SlugRelatedField(read_only=True, slug_field='get_user_username')
+
+    class Meta:
+        model = TicketComment
+        fields = [
+            'public_id', 'ticket', 'commenter',
+            'text', 'posting_date'
         ]
