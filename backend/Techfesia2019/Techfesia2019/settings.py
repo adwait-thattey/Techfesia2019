b3ac673f02a7f398a63ce1a6bfee6c941c065ba1
Ashutosh Chauhan <masterashu@live.in>
2019-06-29 16:18:55 +0530

    Updated Setting: APPEND_SLASH = True to fix api isssue

--- backend/Techfesia2019/Techfesia2019/settings.py
+++ backend/Techfesia2019/Techfesia2019/settings.py
@@ -125,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'registration.User'
 
-APPEND_SLASH = False
+APPEND_SLASH = True
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
