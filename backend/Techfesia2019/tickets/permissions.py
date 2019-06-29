a3cf77fcee67979563629552a76581f0fd1ee926
Ashutosh Chauhan <masterashu@live.in>
2019-06-29 18:03:36 +0530

    Adding Permission: IsStaffuserOrOwner

--- backend/Techfesia2019/tickets/permissions.py
+++ backend/Techfesia2019/tickets/permissions.py
@@ -29,6 +29,11 @@ class IsStaffUserOrPost(permissions.BasePermission):
             return False
 
 
+class IsStaffUserOrOwner(IsStaffUser):
+    def has_object_permission(self, request, view, obj):
+        return super().has_object_permission(request, view, obj) or request.user == obj.owner
+
+
 class IsAuthenticatedOrPost(permissions.IsAuthenticated):
     def has_permission(self, request, view):
         return super().has_permission(request, view) or request.method == 'POST'
