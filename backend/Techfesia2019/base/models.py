49242debc6a463fcc5fdcb01ed71a27b25f74d83
adwait-thattey <coderdude1999@gmail.com>
2019-07-08 18:06:23 +0530

    add file upload model

--- backend/Techfesia2019/base/models.py
+++ backend/Techfesia2019/base/models.py
@@ -1,3 +1,22 @@
+import os
 from django.db import models
 
 # Create your models here.
+from registration.models import User
+
+def set_file_upload_path(instance, filename):
+    if instance.forced_filename:
+        return os.path.join(instance.upload_path, instance.forced_filename)
+    else:
+        return os.path.join(instance.upload_path, filename)
+
+class FileUploadModel(models.Model):
+    """
+    A common model to keep track of all uploads
+    """
+    purpose = models.CharField(max_length=100)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, blank=True)
+    upload_path = models.CharField(max_length=250)
+    forced_filename = models.CharField(max_length=80, null=True, blank=True)
+    uploaded_file = models.FileField(upload_to=set_file_upload_path)
+    additional_info = models.TextField(null=True, blank=True)
