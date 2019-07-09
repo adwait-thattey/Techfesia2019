4ff1a7138d97d1d3cee112cd7e4225de115106de
Ashutosh Chauhan <masterashu@live.in>
2019-07-09 20:55:56 +0530

    Updating Serializer names to even with docs

--- backend/Techfesia2019/tickets/serializers.py
+++ backend/Techfesia2019/tickets/serializers.py
@@ -3,27 +3,32 @@ from .models import TicketComment, Ticket
 
 
 class TicketSerializer(serializers.ModelSerializer):
-    opened_by = serializers.SlugRelatedField(read_only=True, slug_field='get_user_username')
-    solved_by = serializers.SlugRelatedField(read_only=True, slug_field='get_user_username')
+    publicId = serializers.CharField(source='public_id', read_only=True)
+    openedBy = serializers.SlugRelatedField(source='owned_by', read_only=True, slug_field='get_user_username')
+    solvedBy = serializers.SlugRelatedField(source='solved_by', read_only=True, slug_field='get_user_username')
+    openingDate = serializers.DateField(source='opening_date')
+    solvingDate = serializers.DateField(source='solving_date')
     subscribers = serializers.SlugRelatedField(many=True, read_only=True, slug_field='get_user_username')
     event = serializers.SlugRelatedField(read_only=True, slug_field='title')
 
     class Meta:
         model = Ticket
         fields = [
-            'public_id', 'title', 'description',
-            'opened_by', 'opening_date', 'status', 'event',
-            'solved_by', 'solving_date', 'content', 'subscribers'
+            'publicId', 'title', 'description',
+            'openedBy', 'openingDate', 'status', 'event',
+            'solvedBy', 'solvingDate', 'content', 'subscribers'
         ]
 
 
 class TicketCommentSerializer(serializers.ModelSerializer):
+    publicId = serializers.CharField(source='public_id', read_only=True)
     ticket = serializers.SlugRelatedField(read_only=True, slug_field='public_id')
     commenter = serializers.SlugRelatedField(read_only=True, slug_field='get_user_username')
+    postingDate = serializers.DateTimeField(source='posting_date')
 
     class Meta:
         model = TicketComment
         fields = [
-            'public_id', 'ticket', 'commenter',
-            'text', 'posting_date'
+            'publicId', 'ticket', 'commenter',
+            'text', 'postingDate'
         ]
