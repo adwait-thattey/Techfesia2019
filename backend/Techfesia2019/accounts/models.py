78cc25a7fbe2d7bf248950ac4d307030935b23d8
adwait-thattey <coderdude1999@gmail.com>
2019-07-09 14:40:58 +0530

    add signal to create profile

--- backend/Techfesia2019/accounts/models.py
+++ backend/Techfesia2019/accounts/models.py
@@ -2,15 +2,17 @@ from django.core.validators import MinLengthValidator
 from django.db import models
 
 # Create your models here.
+from django.db.models import signals
+from django.dispatch import receiver
+
 from events.models import Event, SoloEvent
 from registration.models import User
-
 
 class Profile(models.Model):
     user = models.OneToOneField(to=User, on_delete=models.CASCADE)
     profile_pic = models.URLField()
-    phone_number = models.CharField(max_length=13, validators=[MinLengthValidator(13)])
-    college_name = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=13, validators=[MinLengthValidator(13)], null=True, blank=True)
+    college_name = models.CharField(max_length=150, null=True, blank=True)
 
 
 class ProfileParticipant(models.Model):
@@ -25,3 +27,15 @@ class ProfileOrganizer(models.Model):
 class ProfileVolunteer(models.Model):
     profile = models.OneToOneField(to=Profile, on_delete=models.CASCADE)
     events = models.ManyToManyField(to=Event, related_name="volunteers")
+
+
+
+@receiver(signals.post_save, sender=User)
+def create_profile_for_user(sender, instance, created, **kwargs):
+    """
+        If user is superuser, then set email confirmed to true
+    """
+
+    if created:
+        profile =Profile(user=instance)
+        profile.save()
