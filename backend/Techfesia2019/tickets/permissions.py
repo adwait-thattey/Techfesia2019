da2598a716c545446dbcee450729b78947dbb103
Ashutosh Chauhan <masterashu@live.in>
2019-07-10 20:51:28 +0530

    Removing excess permission classes

--- backend/Techfesia2019/tickets/permissions.py
+++ backend/Techfesia2019/tickets/permissions.py
@@ -15,33 +15,6 @@ class IsStaffUser(permissions.BasePermission):
             return False
 
 
-class IsStaffUserOrPost(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_staff or request.user.is_superuser or request.method == 'POST':
-            return True
-        else:
-            return False
-
-    def has_permission(self, request, view):
-        if request.user.is_staff or request.user.is_superuser or request.method == 'POST':
-            return True
-        else:
-            return False
-
-
-class IsStaffUserOrOwner(IsStaffUser):
-    def has_object_permission(self, request, view, obj):
-        return super().has_object_permission(request, view, obj) or request.user == obj.owner
-
-
 class IsAuthenticatedOrGet(permissions.IsAuthenticated):
     def has_permission(self, request, view):
         return super().has_permission(request, view) or request.method == 'GET'
-
-
-class IsStaffUserOrOwnerOrPost(IsStaffUserOrPost):
-    def has_permission(self, request, view):
-        return super().has_permission(request, view)
-
-    def has_object_permission(self, request, view, obj):
-        return super().has_object_permission(request, view, obj) or obj.owner == request.user
